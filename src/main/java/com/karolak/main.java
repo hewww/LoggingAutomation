304703f960fbb0aefe4cd883f717c2b2d3bd033c
Hewww <karolak.krzysztof@icloud.com>
2022-04-27 23:37:02 +0200

    Add cucumber package

--- src/main/java/com/karolak/main.java
+++ src/main/java/com/karolak/main.java
@@ -13,28 +13,6 @@ import static com.karolak.testing.config.WebDriverConfig.CHROME;
 public class main {
     public static void main(String[] args) {
 
-        WebDriver driver;
-        driver = WebDriverConfig.getDriver(CHROME);
-        driver.manage().window().maximize();
-        driver.get("https://www.nazwa.pl/zaloguj-sie");
-        WebElement loginField = driver.findElement(By.xpath("//input[@id='clientLogin']"));
-        WebElement passField = driver.findElement(By.xpath("//input[@id='clientPass']"));
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        loginField.clear();
-        passField.clear();
-        loginField.sendKeys("");
-        passField.sendKeys("");
-        passField.submit();
-
-        if (driver.switchTo().alert().getText().contains("")) {
-            System.out.println("GITAAARAAA");
-        }
-        else {
-            System.out.println("xxxx");
-        }
-
-
-
     }
 }
 
